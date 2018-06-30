c2f7f19bc614ec883c80b2d0ac3ba57f51d84707
Talita Oliveira <litaa.oliveira@gmail.com>
2018-06-30 13:28:00 -0300

    Import header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from './Header';
+
 class App extends React.Component {
     render() {
         return (
-            <h1>Componente App!</h1>
+            <Header />
         );
     }
 }
