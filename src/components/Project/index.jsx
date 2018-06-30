391baf27ff668c19d2eaecaf1d1cca4c6df6414b
Talita Oliveira <litaa.oliveira@gmail.com>
2018-06-30 13:44:12 -0300

    Component Project

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Project extends React.Component {
+    render() {
+        return(
+            <div className="center">
+            </div>
+        );
+    }
+}
+
+export default Project;
