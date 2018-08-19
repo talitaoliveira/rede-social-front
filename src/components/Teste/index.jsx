e7c8dcfb4f5471a0294108ba97c8eec5122c6303
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-19 18:58:58 -0300

    Test component

---
+++ src/components/Teste/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Teste extends React.Component {
+
+    render() {
+        return (
+            <div className="center">
+                <h1>Ola mundo - Teste</h1>
+            </div>
+        )
+
+    }
+}
+
+export default Teste;
