7199e5e0faea1559bcc50753520e52a34400222d
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-19 18:57:40 -0300

    Create component ProjectsPage with routes to Projects

---
+++ src/components/PageProjects/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Projects from './Projects';
+import Project from './Project';
+
+const ProjectsPage = () => (
+  <Switch>
+    <Route exact path='/projects' component={Projects}/>
+    <Route path='/projects/:number' component={Project}/>
+  </Switch>
+)
+
+export default ProjectsPage
