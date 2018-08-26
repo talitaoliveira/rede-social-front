c905c45ed3bdad285243a13c952214b68fe43a34
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-26 15:51:54 -0300

    Insert Page404 component in Routes

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom'
 import Home from '../Home';
 import Teste from '../Teste';
 import PageProjects from '../PageProjects';
+import Page404 from '../Page404';
+
 
 const Main = () => (
   <main>
@@ -10,6 +12,7 @@ const Main = () => (
       <Route exact path='/' component={Home}/>
       <Route path='/projects' component={PageProjects}/>
       <Route path='/teste' component={Teste}/>
+      <Route component={Page404} />
     </Switch>
   </main>
 )
