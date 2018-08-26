f8a06359064331629bf1cf634495f23e5e3744d0
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-26 15:51:19 -0300

    Component 404 Page

---
+++ src/components/Page404/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Page202 extends React.Component {
+
+    render() {
+        return (
+            <div className="center">
+                <h1>404 - Página não encontrada</h1>
+            </div>
+        )
+
+    }
+}
+
+export default Page202;
