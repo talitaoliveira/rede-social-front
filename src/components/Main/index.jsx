b6d6ea10e8432f66473d8a88e751ddd5d16efd1d
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-27 01:21:18 -0300

    change name of the route

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from '../Home';
-import Teste from '../Teste';
+import About from '../About';
 import PageProjects from '../PageProjects';
 import Page404 from '../Page404';
-
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/projects' component={PageProjects}/>
-      <Route path='/teste' component={Teste}/>
+      <Route path='/about' component={About}/>
       <Route component={Page404} />
     </Switch>
   </main>
