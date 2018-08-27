59d392874a85fe9d49e86339faeb9cb5726550be
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-27 01:21:43 -0300

    add menu icon on header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
 import './img/logo.png';
+import Menu from '../Menu';
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <div className="header pure-menu-horizontal pure-menu-fixed">
-            <a href="/">
-                <img src="img/logo.png" alt="logo" className="logo" />
-            </a>
+            {/* <img src="img/logo.png" alt="logo" className="logo" /> */}
+            <Menu />
             <h4 className="label">Rede Social</h4>
         </div>
     );
