7b35fba8ad6d8f8ffd2de9a2115e7f8be75bd86f
Talita Oliveira <litaa.oliveira@gmail.com>
2018-08-29 13:00:43 -0300

    remove object from array

--- src/components/PageProjects/Project/index.jsx
+++ src/components/PageProjects/Project/index.jsx
@@ -15,7 +15,7 @@ class Project extends React.Component {
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         let projectId = this.props.match.params.number;
         // Api.getProject(project => {
         //     this.setState({
@@ -24,14 +24,14 @@ class Project extends React.Component {
         // }, projectId);
 
         this.setState({
-            project: [{
+            project: {
                 _id: "5b2e627a8c40531116ed83a7",
                 name: "Carrinho da Alegria",
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 volunteers: true,
                 location: "GAC",
                 donations: true
-            }]
+            }
         });
     }
 
