2f980d5b8f7ffb3b4af4671d47f51c927e7540a5
Talita Oliveira <litaa.oliveira@gmail.com>
2018-09-04 22:34:29 -0300

    Set public path (files of css and js were lost without it)

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,8 @@ module.exports = {
     entry: path.join(__dirname, 'src/index.jsx'),
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     resolve: {
         extensions: [".js", ".jsx"]
