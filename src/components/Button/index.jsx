a84be9de22937d996c6dc54be0d05f5a4320b270
Talita Oliveira <litaa.oliveira@gmail.com>
2018-09-04 22:34:02 -0300

    Button with modal/dialog

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,11 +3,36 @@ import React from 'react';
 export default function Button(props) {
 
     const disabledButton = (props.enabled === false ? 'pure-button-disabled' : '');
+    const favDialog = document.getElementById('favDialog');
+
+    function showModal() {
+        console.log('Show Modal');
+        favDialog.showModal();
+    }
+
+    function cancelModal() {
+        console.log('Cancel Modal');
+        favDialog.close();
+    }
 
     return (
-        <button className={`pure-button button-xlarge ${disabledButton}`}>
-            {props.text}
-        </button>
+        <div>
+            <dialog id="favDialog" className="dialogInfo">
+                <section className="">
+                    <p>Para ser voluntário Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
+                </section>
+                <menu>
+                    <button onClick={() => { cancelModal() }} type="reset">Cancel</button>
+                    <button type="submit">Confirm</button>
+                </menu>
+            </dialog>
+            <button
+                className={`pure-button button-xlarge ${disabledButton}`}
+                onClick={() => { showModal() }}
+            >
+                {props.text}
+            </button>
+        </div>
     )
 
 }
