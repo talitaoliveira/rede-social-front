a9999b75854e80486a1ff06363c8ff702e971496
Talita Oliveira <litaa.oliveira@gmail.com>
2018-10-07 23:41:27 -0300

    Add page contribute to routes

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from '../Home';
 import About from '../About';
+import Contribute from '../Contribute';
 import PageProjects from '../PageProjects';
 import Page404 from '../Page404';
 
@@ -11,6 +12,7 @@ const Main = () => (
       <Route exact path='/' component={Home}/>
       <Route path='/projects' component={PageProjects}/>
       <Route path='/about' component={About}/>
+      <Route path='/contribute' component={Contribute}/>
       <Route component={Page404} />
     </Switch>
   </main>
