033eaf6501ed1166820ffd43a93dfb6847183afc
Talita Oliveira <litaa.oliveira@gmail.com>
2018-10-21 16:08:09 -0300

    - Get image from API - getData =>  Get data from API first, if can't get from repository

--- src/pages/PageProjects/Projects/index.jsx
+++ src/pages/PageProjects/Projects/index.jsx
@@ -29,21 +29,19 @@ class Projects extends React.Component {
     }
 
     getData() {
-        try {
-            this.getDataFromRepository().then(() => {
+        this.getDataFromAPI().then((data) => {
+            console.log('[SUCCESS]: getDataFromAPI');
+            this.saveDataOnRepository(data).then(() => {
+                console.log('[SUCCESS]: saveDataOnRepository');
+            });
+        }).catch(() => {
+            console.log('[APPLICATION ERROR]: Fail to retrieve data from API');
+            this.getDataFromRepository().then((data) => {
                 console.log('[SUCCESS]: getDataFromRepository');
             }).catch(() => {
                 console.warn('[WARNIGN]: Nothing on local repository. GetDataFromAPI');
-                this.getDataFromAPI().then((data) => {
-                    console.log('[SUCCESS]: getDataFromAPI');
-                    this.saveDataOnRepository(data);
-                }).catch(() => {
-                    console.log('[APPLICATION ERROR]: Fail to retrieve data from API');
-                })
-            });
-        } catch (err) {
-            console.error('[APPLICATION ERROR]: ', error);
-        }
+            })
+        });
     }
 
     getDataFromRepository() {
@@ -63,34 +61,44 @@ class Projects extends React.Component {
 
     getDataFromAPI() {
         return new Promise((resolve, reject) => {
-            Api.getProjects(projects => {
-                this.setState({
-                    projects: projects
+            try {
+                Api.getProjects(projects => {
+                    this.setState({
+                        projects: projects
+                    });
+                    resolve(projects);
                 });
-                resolve(projects);
-            });
+            } catch (err) {
+                reject(err);
+            }
         });
     }
 
     saveDataOnRepository(data) {
-        return new Promise((resolve, reject) => {
-            console.log('[INFO]: save data on local repository');
-            repository.save(data, () => {
-                console.log('[SUCCESS]: data saved on local repository.');
-                resolve();
-            });
+        return new Promise(async (resolve, reject) => {
+
+            try{
+                console.log('[INFO]: save data on local repository');
+                repository.save(data, () => {
+                    console.log('[SUCCESS]: data saved on local repository.');
+                    resolve();
+                });
+            } catch (err) {
+                reject(err);
+            }
+            
         });
     }
 
     filter(state, name) {
 
         let projectFiltered = this.state.projects;
-
+        console.log(name)
         projectFiltered = projectFiltered.filter((el) => {
             return el.uf === state
         }).filter((el) => {
             let projectName = el.name.toLowerCase();
-            console.log(name)
+
 
             return projectName.includes(name)
         })
@@ -117,34 +125,34 @@ class Projects extends React.Component {
         let filtenarName = this.state.searchBy.name;
         let stateName = this.state.searchBy.state;
         let resultsFound = this.state.projectsFiltered.length;
-        let {hitSearch} = this.state;
+        let { hitSearch } = this.state;
 
         return (
             <section className="pageSection projectsPage">
                 {hitSearch > 0 &&
-                <div className="projectsPage__filterFeedback">
-                    <span className="filterFeedback__title">Busca realizada:</span>  
-                    <p className="filterFeedback__group">
-                        <strong className="groupFilter__label">Nome: </strong>
-                        <span className="groupFilter__value">{filtenarName ? filtenarName : '-'}</span>
-                    </p>
-                    <p className="filterFeedback__group">
-                        <strong className="groupFilter__label">Estado: </strong>
-                        <span className="groupFilter__value">{stateName ? stateName : '-'}</span>
-                    </p>
-                    <p className="filterFeedback__group">
-                        <strong className="groupFilter__label">Resultados encontrados: </strong>
-                        <span className="groupFilter__value">{resultsFound}</span>
-                    </p>
-                </div>
+                    <div className="projectsPage__filterFeedback">
+                        <span className="filterFeedback__title">Busca realizada:</span>
+                        <p className="filterFeedback__group">
+                            <strong className="groupFilter__label">Nome: </strong>
+                            <span className="groupFilter__value">{filtenarName ? filtenarName : '-'}</span>
+                        </p>
+                        <p className="filterFeedback__group">
+                            <strong className="groupFilter__label">Estado: </strong>
+                            <span className="groupFilter__value">{stateName ? stateName : '-'}</span>
+                        </p>
+                        <p className="filterFeedback__group">
+                            <strong className="groupFilter__label">Resultados encontrados: </strong>
+                            <span className="groupFilter__value">{resultsFound}</span>
+                        </p>
+                    </div>
                 }
-                <Modal handleSelectedFilter={this.handleSelectedFromChild.bind(this)} />
+                <Modal handleSelectedFilter={this.handleSelectedFromChild.bind(this)} projects={projects} />
                 <div className='projectsPage__projectList'>
                     {projects.map((project) => {
                         return (
                             <div className="cardProject" key={project._id}>
                                 <Link to={`/projects/${project._id}`}>
-                                    <img src="https://dummyimage.com/600x400/000/fff" alt="" className="cardProject__image" />
+                                    <img src={project.image} alt="" className="cardProject__image" />
                                     <div className="cardProject__info">
                                         <h2 className="cardProject__title">
                                             {project.name}
