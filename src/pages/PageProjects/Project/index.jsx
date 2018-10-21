c72774b04e69f7238aef9afa28a49f73b5bb0f17
Talita Oliveira <litaa.oliveira@gmail.com>
2018-10-21 16:09:13 -0300

    - Get image from API - Get more info from project - Get data from API first, if can't get from repository

--- src/pages/PageProjects/Project/index.jsx
+++ src/pages/PageProjects/Project/index.jsx
@@ -19,24 +19,28 @@ class Project extends React.Component {
     }
 
     componentWillMount() {
-        this.getData();
+        this.getData().then(() => {
+            console.log('ola')
+        })
     }
 
     getData() {
-        try {
-            this.getDataFromRepository().then(() => {
-                console.log('[SUCCESS]: getDataFromRepository');
-            }).catch(() => {
-                console.warn('[WARNIGN]: Nothing on local repository. GetDataFromAPI');
-                this.getDataFromAPI().then((data) => {
+        return new Promise((resolve, reject) => {
+            try {
+                this.getDataFromAPI().then(() => {
                     console.log('[SUCCESS]: getDataFromAPI');
                 }).catch(() => {
                     console.log('[APPLICATION ERROR]: Fail to retrieve data from API');
-                })
-            });
-        } catch (err) {
-            console.error('[APPLICATION ERROR]: ', error);
-        }
+                    this.getDataFromRepository().then((data) => {
+                    console.log('[SUCCESS]: getDataFromRepository');
+                    }).catch(() => {
+                        console.warn('[WARNIGN]: Nothing on local repository. GetDataFromAPI');
+                    })
+                });
+            } catch (err) {
+                console.error('[APPLICATION ERROR]: ', error);
+            }
+        });
     }
 
     getDataFromRepository() {
@@ -55,26 +59,39 @@ class Project extends React.Component {
     }
 
     getDataFromAPI() {
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
 
             let projectId = this.props.match.params.number;
             Api.getProject(project => {
                 this.setState({
                     project: project
                 });
-                resolve(projects);
+                resolve(project);
             }, projectId);
         });
     }
 
+    formatDescription(description) {
+        let descriptionSplit = description.toUpperCase();
+        // let descriptionFormated;
+        // descriptionSplit.forEach((line)=>{
+        //     descriptionFormated += `${line}\n`;
+        // });
+
+        // return descriptionFormated;
+        return descriptionSplit;
+
+    }
+
     render() {
 
-        let {project} = this.state;
+        let { project } = this.state;
+        //let description = this.formatDescription(project.description);
 
         return (
             <section className="pageSection projectPage">
                 <div className="projectData">
-                    <img src="https://dummyimage.com/600x400/000/fff" alt="" className="projectData__image" />
+                    <img src={project.image} alt="" className="projectData__image" />
                     <h1 className="projectData__title">{project.name}</h1>
                     <div className="projectInfo">
                         <div className="projectInfo__group">
@@ -82,8 +99,26 @@ class Project extends React.Component {
                             <p className="projectInfo__group-value">{project.description}</p>
                         </div>
                         <div className="projectInfo__group">
-                            <strong className="projectInfo__group-label">Localização:</strong>
-                            <p className="projectInfo__group-value">{project.location}</p>
+                            <strong className="projectInfo__group-label">Cidade - Estado:</strong>
+                            <p className="projectInfo__group-value">{project.city} - {project.uf}</p>
+                        </div>
+                        <div className="projectInfo__group">
+                            <strong className="projectInfo__group-label">Instagram:</strong>
+                            <p className="projectInfo__group-value">
+                                <a target="_blank" href="{project.instagram ? project.instagram : '#'}">{project.instagram ? project.instagram : '-'}</a>
+                            </p>
+                        </div>
+                        <div className="projectInfo__group">
+                            <strong className="projectInfo__group-label">Facebook:</strong>
+                            <p className="projectInfo__group-value">
+                                <a target="_blank" href="{project.website ? project.website : '#'}">{project.website ? project.website : '-'}</a>
+                            </p>
+                        </div>
+                        <div className="projectInfo__group">
+                            <strong className="projectInfo__group-label">Website:</strong>
+                            <p className="projectInfo__group-value">
+                                <a target="_blank" href="{project.website ? project.website : '#'}">{project.website ? project.website : '-'}</a>
+                            </p>
                         </div>
                         {/* <div className="projectInfo__group buttonGroup">
                             <Button enabled={project.volunteers} text="Quero ser voluntário!" />
