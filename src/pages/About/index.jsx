d2dd31f87bfdc8363772cfbe29bb1e4110be2105
Talita Oliveira <litaa.oliveira@gmail.com>
2018-10-21 18:40:20 -0300

    Page about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -9,17 +9,62 @@ class About extends React.Component {
     render() {
         return (
             <section className="pageSection aboutPage">
-                <BackButton/>
-                <h2 className="pageSection__title aboutPagePage__title">Sobre este Projeto</h2>
+                <BackButton />
+                <h2 className="pageSection__title">Sobre</h2>
 
                 <article className="pageSection__description">
+                    <h3 className="pageSection__subtitle">A aplicação</h3>
                     <p>
-                        Projeto desenvolvido por <strong>Talita Silva de Oliveira</strong>, com o objetivo de realizar a conclusão da pós graduacão de <strong>Design de Webapps com tecnologia front end</strong>.
-                </p>
+                        Nesta aplicação você pode encontrar algumas ações/projetos sociais. Onde muitas dependem de contribuições e voluntários para se manterem ativas e continuar contribuindo para a sociedade de alguma forma.
+                        <br /><br />
+                        Em cada projeto você escontrará uma descrição e formas de entrar em contato, seja por Instagram, Facebook, Website..
+                    </p>
                 </article>
 
                 <article className="pageSection__description">
-                    <h2 className="pageSection__title">Contato</h2>
+                    <h3 className="pageSection__subtitle">O projeto</h3>
+                    <p>
+                        Este projeto foi desenvolvido por <strong>Talita Silva de Oliveira</strong>, com o objetivo de realizar a conclusão da pós graduacão de <strong>Design de Webapps com tecnologia front end</strong>.
+                    </p>
+                </article>
+
+                <article className="pageSection__description">
+                    <h3 className="pageSection__subtitle">Contato</h3>
+
+                    <ul className="content__social">
+                        
+                        <li className="social-links social-links__github">
+                            <a href="https://github.com/talitaoliveira" target="_blank">
+                            <i className="fab fa-github social-links__icon"></i>
+                            <span className="social-links__url">
+                            https://github.com/talitaoliveira
+                            </span>
+                            </a>
+                        </li>
+                        <li className="social-links social-links__linkedin">
+                            <a href="https://br.linkedin.com/in/litaaoliveira" target="_blank">
+                                <i className="fab fa-linkedin social-links__icon"></i>
+                                <span className="social-links__url">
+                                https://br.linkedin.com/in/litaaoliveira
+                            </span>
+                            </a>
+                        </li>
+
+                        <li className="social-links social-links__linkedin">
+                            <a href="mailto:litaa.oliveira@gmail.com" target="_blank">
+                                <i className="far fa-envelope social-links__icon"></i>
+                                <span className="social-links__url">
+                                Email
+                            </span>
+                            </a>
+                        </li>
+                        
+
+                    </ul>
+                </article>
+
+                {/* <article className="pageSection__description">
+                    <h3 className="pageSection__title">A aplicação</h3>
                     <ul className="content__social">
 
                         <li className="social-links__github">
@@ -49,8 +94,8 @@ class About extends React.Component {
                         </li>
 
                     </ul>
-                </article>
-            </section>
+                </article> */}
+            </section >
         )
 
     }
