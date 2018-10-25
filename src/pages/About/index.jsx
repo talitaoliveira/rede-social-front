11739216cba99ed14c7d1be91cacb0c188d2dcc8
Talita Oliveira <litaa.oliveira@gmail.com>
2018-10-24 23:09:45 -0300

    Remove comented code

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -61,39 +61,6 @@ class About extends React.Component {
 
                     </ul>
                 </article>
-
-                {/* <article className="pageSection__description">
-                    <h3 className="pageSection__title">A aplicação</h3>
-                    <ul className="content__social">
-
-                        <li className="social-links__github">
-                            <a href="https://github.com/talitaoliveira" target="_blank">
-                                <i className="fab fa-github"></i>
-                            </a>
-                        </li>
-                        <li className="social-links__linkedin">
-                            <a href="https://br.linkedin.com/in/litaaoliveira" target="_blank">
-                                <i className="fab fa-linkedin"></i>
-                            </a>
-                        </li>
-                        <li className="social-links__codepen">
-                            <a href="https://codepen.io/talitaoliveira" target="_blank">
-                                <i className="fab fa-codepen"></i>
-                            </a>
-                        </li>
-                        <li className="social-links__medium">
-                            <a href="https://medium.com/@litaoliveira" target="_blank">
-                                <i className="fab fa-medium"></i>
-                            </a>
-                        </li>
-                        <li className="social-links__twitter">
-                            <a href="https://twitter.com/liitacherry" target="_blank">
-                                <i className="fab fa-twitter"></i>
-                            </a>
-                        </li>
-
-                    </ul>
-                </article> */}
             </section >
         )
 
